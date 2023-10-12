f0750a2d16098d3a6ddb994fff1a3d5ee53ad29b
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-12 14:38:20 +0200

    Update F1.java

--- lab2/src/F1.java
+++ lab2/src/F1.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class f1 {
+public class F1 {
     public static void main(String[] args) {
         int[] tab1 = {3,3,-4,5,2,1,0,9,2,4};
         for(int i=tab1.length-1;i>=0;i--)
