1593dde164cf15581d6c8934515db64e6a26392f
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-12 15:12:47 +0200

    F25

---
+++ lab2/src/F25.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class F25 {
+    public static void main(String[] args) {
+        int t1[] = {3,-4,5};
+        int t2[] = {3,-4,5};
+        System.out.println(checkEquality(t1,t2));
+    }
+
+    public static boolean checkEquality(int[] tab1, int[] tab2)
+    {
+        return Arrays.equals(tab1,tab2);
+    }
+}
