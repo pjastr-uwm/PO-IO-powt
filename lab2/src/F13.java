94e4c63844799159b1bc2dd76b1ca7065293aedb
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-12 14:50:20 +0200

    F13

---
+++ lab2/src/F13.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class F13 {
+    public static void main(String[] args) {
+        int tab[] = {4,-5,-9,20};
+        System.out.println(Arrays.toString(tab));
+        System.out.println("Min="+minimumValue(tab));
+    }
+
+    public static int minimumValue(int[] tab)
+    {
+        int temp = tab[0];
+        for(int i=1;i<tab.length;i++)
+        {
+            if (tab[i] < temp)
+            {
+                temp = tab[i];
+            }
+        }
+        return temp;
+    }
+}
