841637dd4313182ab98980a40738d6f038ea3849
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-12 14:38:02 +0200

    F1

---
+++ lab2/src/f1.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class f1 {
+    public static void main(String[] args) {
+        int[] tab1 = {3,3,-4,5,2,1,0,9,2,4};
+        for(int i=tab1.length-1;i>=0;i--)
+        {
+            System.out.println(tab1[i]);
+        }
+        int tab2[] = new int[10];
+        Random random = new Random();
+        random.setSeed(123);
+        for(int i=0;i<tab2.length;i++)
+        {
+            tab2[i] = random.nextInt(101);
+        }
+        for(int i=tab2.length-1;i>=0;i--)
+        {
+            System.out.println(tab2[i]);
+        }
+    }
+}
